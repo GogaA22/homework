2b4459a21f313fc3564f13f9a1082137328949ad
agvan <gogoryan.agvan@gmail.com>
2025-06-26 16:28:14 +0300

    edit procesing.py

--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,15 @@
 from typing import List, Dict
 from datetime import datetime
 
-def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    """
-    Фильтрует список операций по значению поля 'state'.
-
-    :param data: Список словарей с операциями
-    :param state: Значение, по которому нужно фильтровать (по умолчанию 'EXECUTED')
-    :return: Новый список только с операциями нужного статуса
-    """
-    filtered = []
-    for item in data:
-        if item.get("state") == state:
-            filtered.append(item)
-    return filtered
-
-def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
-    """
-    Сортирует список операций по дате.
-
-    :param data: Список словарей с операциями
-    :param reverse: True — сортировка от новых к старым, False — от старых к новым
-    :return: Новый отсортированный список
-    """
-    return sorted(data, key=lambda item: datetime.fromisoformat(item["date"]), reverse=reverse)
+def sort_by_date(data: List[Dict[str, str]], is_descending: bool = True) -> List[Dict[str, str]]:
+   """
+   Сортирует список операций по дате (ISO-формат -> datetime).
+   :param data: Список словарей с операциями
+   :param is_descending: True — сортировка от новых к старым, False — от старых к новым
+   :return: Отсортированный список операций
+   """
+   return sorted(
+     data,
+     key=lambda item: datetime.fromisoformat(item["date"]),
+     reverse=is_descending
+   )
